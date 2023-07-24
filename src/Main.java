9d4b6fc690cfc72421d790cf08d53eef8d59b40e
rcseven <rcedquila@gmail.com>
2023-07-23 18:13:46 -0700

    Add Main Method

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,27 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-  
+        int u1BudgetCost = 0;
+        int u2BudgetCost = 0;
+
+        Simulation simulation = new Simulation();
+        ArrayList<Item> phaseOne = new ArrayList<>();
+        ArrayList<Item> phaseTwo = new ArrayList<>();
+
+        phaseOne.addAll(simulation.loadItems("Phase-1.txt"));
+        phaseOne.addAll(simulation.loadItems("Phase-2.txt"));
+
+        ArrayList<Rocket> u1Rockets = simulation.loadU1(phaseOne);
+        u1Rockets.addAll(simulation.loadU1(phaseTwo));
+
+        ArrayList<Rocket> u2Rockets = simulation.loadU2(phaseOne);
+        u2Rockets.addAll(simulation.loadU2(phaseTwo));
+
+        u1BudgetCost += simulation.runSimulation(u1Rockets);
+        u2BudgetCost += simulation.runSimulation(u2Rockets);
+
+        System.out.println("U1 budget cost: " + u1BudgetCost);
+        System.out.println("U2 budget cost: " + u2BudgetCost);
     }
 }
